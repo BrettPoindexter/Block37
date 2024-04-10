002478f3d455ac1f543cbd74bb53c13add0475b1
BrettPoindexter <poindexterbrett@gmail.com>
2024-04-10 16:01:37 -0700

    Working on API

--- test/submitReview.test.js
+++ test/submitReview.test.js
@@ -1,11 +1,6 @@
 const db = require('./db');
-const { writeReview } = require('./review');
+const { all } = require('./review');
 
 jest.mock('./db');
 
-describe('writeReview', () => {
-    it('should create and post a review to the database', async () => {
-        const mockWriteReview = 'This is a test of the writeReview function';
-        db.query.mockWriteReview({ rows: [{ post: mockWriteReview }] });
-    })
-})
+describe
